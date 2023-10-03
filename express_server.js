209c0cc25bee93ff497a41aa5853a0593331faf6
Gregory Dechant <dechantg@gmail.com>
2023-10-03 00:03:22 +0000

    moving onto template enjine exercise

--- express_server.js
+++ express_server.js
@@ -27,3 +27,11 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html)\n");
 });
 
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
