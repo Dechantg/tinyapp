6269358fa5cddcfeefa34f3973f8e3169c5bead4
Gregory Dechant <dechantg@gmail.com>
2023-10-04 22:12:46 +0000

    finished up all of the tuesday tasks

--- express_server.js
+++ express_server.js
@@ -1,16 +1,32 @@
 
 
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
-
+const cookieParser = require("cookie-parser");
 const PORT = 8080;
+app.set("view engine", "ejs", "cookie-parser");
 
-app.set("view engine", "ejs");
+
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+
+
+
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+
+  const templateVars = { 
+    username: req.cookies["username"],
+    urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+// // const username = {};
 
 const urlDatabase = {
   "b2xVn2" : "http://www.lighthouselabs.ca",
@@ -18,7 +34,7 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  res.redirect("/urls");
 
 });
 
@@ -28,26 +44,48 @@ app.listen(PORT, () => {
 
 // 6 string randome key generator for URL keys
 const generateRandomString = () => {
-  //list of valid letters to use
-  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  let randomString = "";
-  // loop through with math random and push 6 random characters to return
-  for (let i = 0; i < 6; i++) {
-    const list = Math.floor(Math.random() * letters.length);
-    randomString += letters.charAt(list);
-  }
+  //list of valid letters to use using what andy showed in lecture
+  const randomString = Math.random().toString(36).substring(2, 8);
+  console.log(`Random String for shortURL: ${randomString}`);
+  
   return randomString;
 
 };
 
-app.use(express.urlencoded({ extended: true }));
+
+
+app.post("/login", (req, res) => {
+  console.log(req.body.username);
+  const username = req.body.username;
+
+  res.cookie("username", username);
+    res.redirect("/urls");
+
+});
+
+
 
 app.post("/urls", (req, res) => {
+  console.log(req.body.username); // Log the POST request body to the console
+  
+
+  res.redirect(`/urls`); 
+});
+
+
+
+
+app.post("/urls_add", (req, res) => {
+
   console.log(req.body); // Log the POST request body to the console
   const key = generateRandomString(); // use my new random key generator
   urlDatabase[key] = req.body.longURL; // push randome key and url
-  res.redirect(`/urls/${key}`); //redirect to urls/key
+  const username = req.cookies.username;
+ 
 });
+
+
+
 
 app.post("/urls/:id/edit", (req, res) => {
   const editID = req.params.id;
@@ -59,13 +97,18 @@ app.post("/urls/:id/edit", (req, res) => {
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 
-
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+   };
   res.render("urls_show", templateVars);
 });
 
@@ -77,10 +120,20 @@ app.post("/urls/:id/delete", (req, res) => {
   res.redirect("/urls");
 });
 
-// create call for users to use the short url key to go straight to the webpage
+// // create call for users to use the short url key to go straight to the webpage
 app.get("/u/:id", (req, res) => {
   const key = req.params.id; // pull the key from the reqest
   const longURL = urlDatabase[key]; // pull the URL based on the key given
   res.redirect(longURL); // redirect to the url
 
 });
+
+
+app.post("/logout", (req, res) => {
+  console.log(req.body.username);
+
+  res.clearCookie("username");
+    res.redirect("/urls");
+
+});
+
